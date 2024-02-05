2f76be14915677a3139923ad9853e2ab61b349e2
Kade Hemmerling <157063757+kadeanater@users.noreply.github.com>
2024-02-05 17:51:48 -0600

    commit

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -4,6 +4,6 @@ def telephone():
     area_code=phone_number // 10000000
     digit_456= phone_number // 10000 % 1000
     digit_678= phone_number % 10000
-    
+    print(f"({area_code}) {digit_456}-{digit_678}")
 if __name__ == "__main__":
     telephone()
--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -2,15 +2,11 @@
 def caffeine():
     caffeine_mg = float(input())
     ''' Type your code here. '''
-    caffeine_halflife_6hrs= float(caffeine_mg / 2)
-    caffeine_halflife_12hrs=float(caffeine_halflife_6hrs / 2)
-    caffeine_halflife_24hrs=float(caffeine_halflife_12hrs / 2)
+    caffeine_halflife_6hrs= caffeine_mg*(0.5**(6/6))
+    caffeine_halflife_12hrs= caffeine_mg*(0.5**(12/6))
+    caffeine_halflife_24hrs= caffeine_mg*(0.5**(24/6))
 
-    print('After 6 Hours:')
-    print(caffeine_halflife_6hrs)
-    print('After 12 Hours:')
-    print(caffeine_halflife_12hrs)
-    print('After 24 Hours:')
-    print(caffeine_halflife_24hrs)
+    print(f'After 6 hours: {caffeine_halflife_6hrs:.2f} mg\n'f'After 12 hours: {caffeine_halflife_12hrs:.2f} mg\n'f'After 24 hours: {caffeine_halflife_24hrs:.2f} mg')
+   
 if __name__ == "__main__":
     caffeine()
