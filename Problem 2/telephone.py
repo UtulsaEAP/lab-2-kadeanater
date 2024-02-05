760f1176336bdcb57bb449cefb9699d4f04b3ab0
Kade Hemmerling <157063757+kadeanater@users.noreply.github.com>
2024-02-05 15:30:32 -0600

    commit

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,5 +1,9 @@
 def telephone():
     phone_number = int(input())
     ''' Type your code here. '''
+    area_code=phone_number // 10000000
+    digit_456= phone_number // 10000 % 1000
+    digit_678= phone_number % 10000
+    
 if __name__ == "__main__":
     telephone()
--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -2,6 +2,15 @@
 def caffeine():
     caffeine_mg = float(input())
     ''' Type your code here. '''
-    
+    caffeine_halflife_6hrs= float(caffeine_mg / 2)
+    caffeine_halflife_12hrs=float(caffeine_halflife_6hrs / 2)
+    caffeine_halflife_24hrs=float(caffeine_halflife_12hrs / 2)
+
+    print('After 6 Hours:')
+    print(caffeine_halflife_6hrs)
+    print('After 12 Hours:')
+    print(caffeine_halflife_12hrs)
+    print('After 24 Hours:')
+    print(caffeine_halflife_24hrs)
 if __name__ == "__main__":
     caffeine()
