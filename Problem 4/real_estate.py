d788120529cf416436588663127e5c6cac51c728
Kade Hemmerling <157063757+kadeanater@users.noreply.github.com>
2024-02-08 14:13:18 -0600

    commit

--- Problem 4/real_estate.py
+++ Problem 4/real_estate.py
@@ -4,6 +4,7 @@ def real_estate():
     current_price = int(input())
     last_month_price = int(input())
     # Your code goes here
-    print(f'This house is ${(current_price)}. The change is ${(current_price-last_month_price)} since last month.\nThe estimated mothly mortgage is ${((current_price* 0.051) / 12):.2f}.')
+    print(f'This house is ${(current_price)}. The change is ${(current_price-last_month_price)} since last month.') 
+    print(f'The estimated monthly mortgage is ${((current_price* 0.051) / 12):.2f}.')
 if __name__ == "__main__":
     real_estate()
